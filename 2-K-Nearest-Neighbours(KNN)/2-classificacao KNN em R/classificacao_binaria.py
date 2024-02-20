ca2f11f476419456995da729b26426e80429bd12
Frederico Tavares <fredericotp@hotmail.com>
2024-02-20 12:01:44 -0300

    Feat: Classificação binária em Python

--- 2-K-Nearest-Neighbours(KNN)/2-classificacao KNN em R/classificacao_binaria.py
+++ 2-K-Nearest-Neighbours(KNN)/2-classificacao KNN em R/classificacao_binaria.py
@@ -1,85 +1,9 @@
-# import numpy as np
-# import pandas as pd
-# from sklearn.model_selection import train_test_split
-# from sklearn.preprocessing import StandardScaler
-# from sklearn.neighbors import KNeighborsClassifier
-# from sklearn.metrics import confusion_matrix
-# import warnings
-
-# warnings.filterwarnings("ignore")
-
-# # Carregando o dataset
-# Smarket = pd.read_csv(
-#     "https://raw.githubusercontent.com/vincentarelbundock/Rdatasets/master/csv/ISLR/Smarket.csv"
-# )
-# print(Smarket)
-
-# # Gera X e Y
-# X = Smarket.drop(columns=["Direction"])
-# Y = Smarket["Direction"]
-
-# # Divisão em dados de treino e de teste
-# X_treino, testeData, Y_treino, testelabels = train_test_split(
-#     X, Y, test_size=0.30, random_state=300
-# )
-
-# # Divisão dos dados de treino em dados de treino e dados de validação
-# treinoData, validData, treinoLabels, validLabels = train_test_split(
-#     X_treino, Y_treino, test_size=0.1, random_state=84
-# )
-
-# # Normalização dos dados
-# scaler = StandardScaler()
-# X_treino_norm = scaler.fit_transform(treinoData)
-# X_valid_norm = scaler.transform(validData)
-# X_teste_norm = scaler.transform(testeData)
-
-# # Shape dos datasets
-# print("Shape datasets: ", X_treino_norm.shape, X_valid_norm.shape, X_teste_norm.shape)
-
-# # Testando o melhor valor de K
-# # Range de valores de k que iremos testar
-# kVals = range(1, 150, 2)
-
-# # Lista vazia para receber as acurácias
-# acuracias = []
-
-# # Loop em todos os valores de k para testar cada um deles
-# for k in kVals:
-#     # Treinando o modelo KNN com cada valor de k
-#     modeloKNN = KNeighborsClassifier(n_neighbors=k)
-#     modeloKNN.fit(treinoData, treinoLabels)
-
-#     # Avaliando o modelo e atualizando a lista de acurácias
-#     score = modeloKNN.score(X_valid_norm, validLabels)
-#     print("Com valor de k = %d, a acurácia é = %.2f%%" % (k, score * 100))
-#     acuracias.append(score)
-
-# # Obtendo o valor de K que paresentou a maior acurácia
-# i = np.argmax(acuracias)
-# print(
-#     "O valor de k = %d alcançou a mais alta acurácia de %.2f%% nos dados de validação!"
-#     % (kVals[i], acuracias[i] * 100)
-# )
-
-# # Construção e treinamento do Modelo KNN
-# # Criando a versão final do modelo com o maior valor de k
-# modeloFinal = KNeighborsClassifier(n_neighbors=kVals[i])
-
-# # Treinamento do modelo
-# modeloFinal.fit(treinoData, treinoLabels)
-
-# # Previsões com Dados de Teste e Avaliação do Modelo
-# # Previsões com os dados de teste
-# predictions = modeloFinal.predict(testeData)
-
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix, classification_report
-import matplotlib.pyplot as plt
 import warnings
 
 warnings.filterwarnings("ignore")
