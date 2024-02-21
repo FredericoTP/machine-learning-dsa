ea119c27eea89fa82996aa5e60f5f0879d10cc37
Frederico Tavares <fredericotp@hotmail.com>
2024-02-21 10:28:47 -0300

    Feat: Arquivos para realização do exercício

---
+++ 2-K-Nearest-Neighbours(KNN)/exercicio-1/exercicio.py
@@ -0,0 +1,7 @@
+# O exercício consiste em desenvolver o algoritmo KNN usando apenas
+# linguagem Python e Numpy sem a utilização de frameworks.
+
+# No dataset fornecido, cada planta possui 4 variáveis preditoras
+# e uma variável representando a classe.
+
+# Seu algoritmo KNN deve prever a classe de uma nova planta.
