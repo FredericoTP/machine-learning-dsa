0744850b6aeb38888c0410d5acf7e5eb980569ee
Frederico Tavares <fredericotp@hotmail.com>
2024-04-17 11:18:00 -0300

    Feat: Criando Árvore de Decisão em Python baseada em Índice Gini

--- 5-Decision-Tree-Random-Forest-e-Metodos-Ensemble-Parte-2/1-Arvore-de-decisao-em-Python/arvore-de-decisao.py
+++ 5-Decision-Tree-Random-Forest-e-Metodos-Ensemble-Parte-2/1-Arvore-de-decisao-em-Python/arvore-de-decisao.py
@@ -57,3 +57,28 @@ with open(arquivo) as f:
 graphviz.Source(dot_graph)
 
 # !dot -Tpng tree_modelo_v1.dot -o tree_modelo_v1.png
+
+
+# Construindo o Classificador Baseado no índice Gini
+modelo_v2 = DecisionTreeClassifier(
+    max_depth=None,
+    max_features=None,
+    min_samples_leaf=1,
+    min_samples_split=2,
+)
+
+# Apresentando os dados ao Classificador
+modelo_v2.fit(X_train, y_train)
+
+# Definindo o nome do arquivo com a árvore de decisão
+diretorio_atual = Path().resolve()
+arquivo = str(diretorio_atual) + "/tree_modelo_v2.dot"
+print(arquivo)
+
+# Gerando o gráfico da árvore de decisão
+export_graphviz(modelo_v2, out_file=arquivo, feature_names=labels)
+with open(arquivo) as f:
+    dot_graph = f.read()
+graphviz.Source(arquivo)
+
+# !dot -Tpng tree_modelo_v2.dot -o tree_modelo_v2.png
