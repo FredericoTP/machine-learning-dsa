50f895adcf4bd352f6209cfc782c9e1b0aede1d7
Frederico Tavares <fredericotp@hotmail.com>
2024-05-10 13:58:03 -0300

    Feat: SVM em R

---
+++ 1-Aprendizagem-Supervisionada/6-Classificacao-e-Regressao-com-Support-Vector-Machines-SVMs/1-Classificação-Multiclasse-comSVM-em-R/1-analise-exploratoria.R
@@ -0,0 +1,58 @@
+# Classificação Multiclasse com SVM - Prevendo Gastos com Cartão de Crédito em 3 Categorias
+
+# Definindo o diretório de trabalho
+getwd()
+setwd("resposta getwd()")
+
+# Pacotes
+install.packages("gains")
+install.packages("pROC")
+install.packages("ROSE")
+install.packages("mice")
+library(dplyr)
+library(caret)
+library(gains)
+library(pROC)
+library(ROCR)
+library(ROSE)
+library(e1071)
+library(mice)
+
+# Carregando os dados
+dataset_clientes <- read.csv("dados/cartoes_clientes.csv")
+View(dataset_clientes)
+
+
+#### Análise Exploratória dos Dados #### 
+str(dataset_clientes)
+summary(dataset_clientes)
+summary(dataset_clientes$card2spent)
+
+# Removemos a variável com ID do cliente pois não é necessário
+dataset_clientes <- dataset_clientes[-1]
+View(dataset_clientes)
+
+# Checando valores missing
+sapply(dataset_clientes, function(x)sum(is.na(x)))
+
+# Checando se a variável alvo está balanceada
+table(dataset_clientes$Customer_cat)
+prop.table(table(dataset_clientes$Customer_cat)) * 100
+
+# Outra alternativa
+as.data.frame(table(dataset_clientes$Customer_cat))
+
+
+# Análise Visual
+
+# BoxPlot e Histograma
+boxplot(dataset_clientes$card2spent)
+summary(dataset_clientes$card2spent)
+hist(dataset_clientes$card2spent)
+
+boxplot(dataset_clientes$hourstv)
+summary(dataset_clientes$hourstv)
+hist(dataset_clientes$hourstv)
+
+# Scatter Plot
+plot(dataset_clientes$card2spent, dataset_clientes$hourstv, xlab = "Gasto Cartão", ylab = "Horas TV")
